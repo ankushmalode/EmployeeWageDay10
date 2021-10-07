d75172821c81eb4abeda4fe4b3fd12b5b18251bf
ankushmalode <you@example.com>
2021-10-07 14:42:57 +0530

    Calculating daily employee wage

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -7,17 +7,21 @@ public class EmployeeWage {
     public static final int isPartTime = 2;
 
 
-    public static void wageCalculation(){
-        int totalHr=0;
-        Random num= new Random();
-        int attendance= num.nextInt(2);
-        switch (attendance){
+    public static void wageCalculation() {
+        int totalHr = 0, fullDayHr = 8, wagePerHr = 20;
+        Random num = new Random();
+        int attendance = num.nextInt(2);
+        switch (attendance) {
             case isFullTime:
                 System.out.println("Employee is present");
+                totalHr = fullDayHr;
                 break;
             default:
                 System.out.println("Employee is Absent");
+                totalHr = 0;
+                break;
         }
-    }
 
+        System.out.println("Full day employee wage is " + totalHr * wagePerHr);
+    }
 }
--- src/main/java/com/bridgelabz/Program.java
+++ src/main/java/com/bridgelabz/Program.java
@@ -8,7 +8,7 @@ package com.bridgelabz;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        EmployeeWage employeeWage=new EmployeeWage();
+        EmployeeWage employeeWage = new EmployeeWage();
         employeeWage.wageCalculation();
     }
 }
